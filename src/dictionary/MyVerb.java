54311b3b0408e82523cb20e48f0fb439bce33cbb
FedeRio-Biscotto <66733146+FedeRio-Biscotto@users.noreply.github.com>
2025-05-20 16:22:22 +0200

    Update MyVerb.java
    commenti

--- src/dictionary/MyVerb.java
+++ src/dictionary/MyVerb.java
@@ -2,14 +2,17 @@
 package dictionary;
 
 public class MyVerb extends Token {
+    //Costruttore
     public MyVerb (String v){
         super(v);
     }
 
+    //Metodo GET
     public String getMyVerb(){
         return this.getToken();
     }
 
+    //Metodo SET
     public void setMyVerb(String n){
         this.setToken(n);
     }
