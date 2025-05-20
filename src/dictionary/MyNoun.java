dcf2a9eb3a4f24241e7b4c3ee776aa2e01faf29f
FedeRio-Biscotto <66733146+FedeRio-Biscotto@users.noreply.github.com>
2025-05-20 16:21:37 +0200

    Update MyNoun.java
    commenti

--- src/dictionary/MyNoun.java
+++ src/dictionary/MyNoun.java
@@ -2,14 +2,17 @@
 package dictionary;
 
 public class MyNoun extends Token{
+    //Costruttore
     public MyNoun(String n){
         super(n);
     }
-
+    
+    //Metodo GET
     public String getMyNoun(){
         return this.getToken();
     }
 
+    //Metodo SET
     public void setMyNoun(String n){
         this.setToken(n);
     }
