e91170302358005d67b787dffa56813dcf6bee94
FedeRio-Biscotto <66733146+FedeRio-Biscotto@users.noreply.github.com>
2025-05-20 16:20:20 +0200

    Update Dictionary.java
    commenti

--- src/dictionary/Dictionary.java
+++ src/dictionary/Dictionary.java
@@ -7,13 +7,8 @@ import java.io.IOException;
 import java.util.*;
 import java.util.function.Function;
 
-/*intanto vi carico una prima versione del dizionario,
- * bisogna commentare il codice per bene e
- * cancellare alcuni metodi tipo test
- */
-
 public class Dictionary  {
-    //Costruttore
+    //--------------------------------COSTRUTTORE--------------------------------//
     public Dictionary() throws IOException{
         nouns=setList("src/data/Nouns.txt", MyNoun::new);
 
@@ -24,23 +19,21 @@ public class Dictionary  {
         adjs=setList("src/data/Adjs.txt", MyAdjective::new);
     }
  
-    //metodo usato nel costruttore
+    //Legge il file riga per riga e trasforma ogni riga in un oggetto di tipo "T" usando una funzione 'creator' e restituisce la lista di oggetti
     private static <T> List<T> setList(String fileName, Function<String, T> creator) throws IOException{
         FileReader file=new FileReader(fileName);
         Scanner sc=new Scanner(file);
         List<T> result=new ArrayList<>();
         while (sc.hasNextLine()) {
             String line=sc.nextLine();
-            result.add(creator.apply(line));
+            result.add(creator.apply(line));    //Converte ogni riga del file in un oggetto T
         }
 
         sc.close();
         return result;
     }
-
-    //Aggiungere verbi in terza persona e past
     
-//Metodi get
+//--------------------------------METODI GET--------------------------------//
     public MyNoun getNoun(){
         return chooseToken(nouns);
     }
@@ -68,17 +61,16 @@ public class Dictionary  {
         return t;
     }
 
-// METODI DI DEBUG  
+//--------------------------------METODI DI DEBUG--------------------------------// 
 
-    //Debug: metodo print
+    //[Debug]: metodo print
     private static <T> void printList(List<T> list){
         for (T t : list) {
             System.out.println(t);
         }
     }
 
-
-    //Variabili
+//--------------------------------VARIABILI--------------------------------//
     private List<MyNoun> nouns;
     private List<MyVerb> verbs, verbs_nothirdperson, verbs_past;
     private List<MyAdjective> adjs;
